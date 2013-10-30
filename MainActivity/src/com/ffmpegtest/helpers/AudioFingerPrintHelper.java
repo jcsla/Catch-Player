a98c109fd27c4ede030ec2b7b5c6524e7390f893
Jun-Young Lee <jun-young@Developer.(none)>
2013-10-31 03:21:42 +0900

    set class audiofingerprint

--- MainActivity/src/com/ffmpegtest/helpers/AudioFingerPrintHelper.java
+++ MainActivity/src/com/ffmpegtest/helpers/AudioFingerPrintHelper.java
@@ -1,10 +1,18 @@
 package com.ffmpegtest.helpers;
 
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
+import android.os.AsyncTask;
 import android.os.Environment;
+
+import com.ffmpegtest.*;
+import com.appunite.ffmpeg.FFmpegPlayer;
 
 public class AudioFingerPrintHelper
 {
@@ -31,20 +39,91 @@ public class AudioFingerPrintHelper
 		cmd.add(videoFilePath);
 		cmd.add("-ac");
 		cmd.add("1");
-		cmd.add("-f");
-		cmd.add("s16le");
 		cmd.add("-ar");
 		cmd.add("2000");
+		cmd.add("-f");
+		cmd.add("s16le");
 		cmd.add("-t");
-		cmd.add("0");
-		cmd.add("-ss");
 		cmd.add("120");
-		cmd.add("-acodec");
-		cmd.add("pcm_s16le");
+		cmd.add("-ss");
+		cmd.add("0");
 		cmd.add(path + "/audioData");
 		
 		final ProcessBuilder pb = new ProcessBuilder(cmd);
 		
 		return new ProcessRunnableHelper(pb);
+	}
+	
+	public static void startAudioFingerPrint()
+	{
+		final AudioFingerPrintHelper fingerprint = new AudioFingerPrintHelper(MainActivity.mFFmpegInstallPath, VideoActivity.path);
+		
+		
+		new AsyncTask<Void, Void, Void>() {
+
+			@Override
+			protected Void doInBackground(Void... arg) {
+				//fingerprint.create().run();
+				//readAudioDataFile();
+				return null;
+			}
+			
+			@Override
+			protected void onPostExecute(Void result) {
+				System.out.println("onPostExcute");
+				
+				// param.h main.cxx
+				// 여기서 데이터 읽어서 코드젠으로 변환 후 json 형태로 서버에 전송!!!
+				//String path = Environment.getExternalStorageDirectory() + "/android/data";
+				//File listFile = new File(path);
+				//for(File f : listFile.listFiles()) {
+				//	String str = f.getName();
+				//	System.out.println(str);
+				//}
+				//readAudioDataFile();
+				//System.out.println(data[0]);
+				//String s = mMpegPlayer.codegen(data, data.length);
+				//System.out.println(s);
+			}
+
+		}.execute();
+	}
+	
+	public float[] readAudioDataFile()
+	{
+		File file = new File(Environment.getExternalStorageDirectory() + "/android/data/audioData");
+		InputStream in = null;
+
+		if (file.isFile())
+		{
+			long size = file.length();
+			System.out.println(size);
+			try {
+				in = new FileInputStream(file);
+				return readStreamAsDoubleArray(in, size);
+			} catch (Exception e) {
+
+			}
+		}
+		return null;
+	}
+	
+	public float[] readStreamAsDoubleArray(InputStream in, long size)
+	{
+		int bufferSize = (int) (size / 2);
+		float[] result = new float[bufferSize];
+		DataInputStream is = new DataInputStream(in);
+		
+		for (int i = 0; i < bufferSize; i++) {
+			try {
+				result[i] = is.readShort() / Short.MAX_VALUE;
+				//System.out.println(result[i]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		String s = VideoActivity.mMpegPlayer.codegen(result, bufferSize);
+		System.out.println(s);
+		return result;
 	}
 }
